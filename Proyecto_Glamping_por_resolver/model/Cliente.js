8ba3842d22ef8c7c53259eb10518e19c553f7277
5h4rkYCode <sebasgarciag1028@gmail.com>
2025-04-15 12:44:08 -0500

    Se agrego un ciclo que permite identificar un cliente a traves del ID ingresado

--- Proyecto_Glamping_por_resolver/model/Cliente.js
+++ Proyecto_Glamping_por_resolver/model/Cliente.js
@@ -193,16 +193,17 @@ class Cliente {
         const clientes = Cliente.obtenerClientes();
         
         // Recorrer el array de clientes para encontrar el que coincida con el ID
-        /**
-         * 
-         * 
-         * Aqui va la implementacion de la busqueda del cliente por ID usando un ciclo
-         * 
-         * 
-         */
-        
-        // Si no se encuentra, retornar null
-        return null;
+           
+         
+         
+         // Aqui va la implementacion de la busqueda del cliente por ID usando un ciclo
+         for ( let i =0; i < clientes.length; i++){
+            if (clientes[i].getId === id){
+                return clientes[i];
+            }
+        }
+          // Si no se encuentra, retornar null
+         return null;
     }
 
     /**
